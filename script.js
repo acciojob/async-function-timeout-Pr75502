d14fb40981aaeff8c0c6d62296896e54a4cc3289
Pr75502 <pp1614582@gmail.com>
2025-02-22 16:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let text1 = document.getElementById("text")
+let delay = document.getElementById("delay")
+let btn = document.getElementById("btn")
+let output = document.getElementById("output")
+output.innertext=""
+btn.addEventListener("click", display)
+async function display() {
+    let promise = new Promise((resolve) => {
+         setTimeout(()=> {
+        resolve(text1.value)
+     },Number(delay.value))
+    })
+    let result = await promise
+    console.log(result)
+    let p = document.createElement("p")
+    p.innerText=result
+   output.appendChild(p)
+}
